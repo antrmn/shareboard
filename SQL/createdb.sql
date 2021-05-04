52dc576d6dc55edbce2e53d1206f6ea0be51bd14
Antonio <ar0x2ff@gmail.com>
2021-05-04 12:01:44 +0200

    Schema refactor

--- SQL/createdb.sql
+++ SQL/createdb.sql
@@ -1,65 +1,208 @@
-DROP SCHEMA IF EXISTS ShareBoard;
-CREATE SCHEMA IF NOT EXISTS ShareBoard;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: shareboard
+-- ------------------------------------------------------
+-- Server version	8.0.22
 
-use ShareBoard;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE user (
-	id int AUTO_INCREMENT PRIMARY KEY,
-    username varchar(30) UNIQUE NOT NULL,
-    password varchar(255) NOT NULL,
-    email varchar(50) UNIQUE NOT NULL,
-    creation_date TIMESTAMP DEFAULT NOW()
-);
+USE shareboard;
 
-CREATE TABLE admin (
-	admin_id int PRIMARY KEY,
-    FOREIGN KEY(admin_id) REFERENCES user(id) ON DELETE CASCADE
-);
+--
+-- Table structure for table `admin`
+--
 
-CREATE TABLE category (
-	id int AUTO_INCREMENT PRIMARY KEY,
-    description VARCHAR(100),
-    name varchar(50) UNIQUE NOT NULL
-);
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+                         `user_id` int NOT NULL,
+                         PRIMARY KEY (`user_id`),
+                         CONSTRAINT `fk_admin_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE post (
-	post_id int AUTO_INCREMENT PRIMARY KEY,
-    category_id int NOT NULL,
-	author_id int NOT NULL,
-    title VARCHAR(100) NOT NULL,
-    text VARCHAR(1000) NOT NULL,
-	#karma int DEFAULT 0,
-    type VARCHAR(8) DEFAULT 'TEXT',
-	creation_date TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY(author_id) REFERENCES user(id),
-    FOREIGN KEY(category_id) REFERENCES category(id) ON DELETE CASCADE
-);
+--
+-- Table structure for table `ban`
+--
 
-CREATE TABLE comment(
-	id int AUTO_INCREMENT PRIMARY KEY,
-    post_id int NOT NULL,
-	author_id int NOT NULL,
-    text varchar(1000) NOT NULL,
-    #karma int DEFAULT 0,
-    creation_date TIMESTAMP DEFAULT NOW(),
-	FOREIGN KEY(author_id) REFERENCES user(id),
-    FOREIGN KEY(post_id) REFERENCES post(post_id) ON DELETE CASCADE
-);
+DROP TABLE IF EXISTS `ban`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ban` (
+                       `id` int NOT NULL AUTO_INCREMENT,
+                       `admin_id` int DEFAULT NULL,
+                       `section_id` int DEFAULT NULL,
+                       `user_id` int NOT NULL,
+                       `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                       `end_time` timestamp NULL DEFAULT NULL,
+                       `is_global` tinyint(1) NOT NULL DEFAULT '1',
+                       PRIMARY KEY (`id`),
+                       KEY `section_id` (`section_id`),
+                       KEY `fk_ban_admin_id` (`admin_id`),
+                       KEY `fk_ban_user_id` (`user_id`),
+                       CONSTRAINT `fk_ban_admin_admin_id` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`user_id`) ON DELETE RESTRICT,
+                       CONSTRAINT `fk_ban_section_section_id` FOREIGN KEY (`section_id`) REFERENCES `section` (`id`) ON DELETE CASCADE,
+                       CONSTRAINT `fk_ban_user_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+                       CONSTRAINT `global_or_local` CHECK ((((`is_global` = 1) and (`section_id` is null)) or ((`is_global` = 0) and (`section_id` is not null))))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE postVotes (
-	post_id int NOT NULL,
-    user_id int NOT NULL,
-    vote tinyint NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES user(id),
-    FOREIGN KEY(post_id) REFERENCES post(post_id) ON DELETE CASCADE,
-    PRIMARY KEY(post_id, user_id)
-);
+--
+-- Table structure for table `comment`
+--
 
-CREATE TABLE commentVotes (
-	comment_id int NOT NULL,
-    user_id int NOT NULL,
-    vote tinyint NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES user(id),
-    FOREIGN KEY(comment_id) REFERENCES comment(id) ON DELETE CASCADE,
-    PRIMARY KEY(comment_id, user_id)
-);
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment` (
+                           `id` int NOT NULL AUTO_INCREMENT,
+                           `post_id` int NOT NULL,
+                           `author_id` int NOT NULL,
+                           `parent_comment_id` int DEFAULT NULL,
+                           `content` text NOT NULL,
+                           `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                           PRIMARY KEY (`id`),
+                           KEY `author_id` (`author_id`),
+                           KEY `post_id` (`post_id`),
+                           KEY `parent_comment_id` (`parent_comment_id`),
+                           CONSTRAINT `fk_comment_comment_parent_comment_id` FOREIGN KEY (`parent_comment_id`) REFERENCES `comment` (`id`) ON DELETE CASCADE,
+                           CONSTRAINT `fk_comment_post_post_id` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE,
+                           CONSTRAINT `fk_comment_user_author_id` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `comment_vote`
+--
+
+DROP TABLE IF EXISTS `comment_vote`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment_vote` (
+                                `comment_id` int NOT NULL,
+                                `user_id` int NOT NULL,
+                                `vote` tinyint NOT NULL,
+                                PRIMARY KEY (`comment_id`,`user_id`),
+                                KEY `user_id` (`user_id`),
+                                CONSTRAINT `fk_comment_vote_comment_comment_id` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`id`) ON DELETE CASCADE,
+                                CONSTRAINT `fk_comment_vote_user_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `follow`
+--
+
+DROP TABLE IF EXISTS `follow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `follow` (
+                          `user_id` int NOT NULL,
+                          `section_id` int NOT NULL,
+                          PRIMARY KEY (`user_id`,`section_id`),
+                          KEY `section_id` (`section_id`),
+                          CONSTRAINT `fk_follow_section_section_id` FOREIGN KEY (`section_id`) REFERENCES `section` (`id`) ON DELETE CASCADE,
+                          CONSTRAINT `fk_follow_user_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post` (
+                        `id` int NOT NULL AUTO_INCREMENT,
+                        `section_id` int NOT NULL,
+                        `author_id` int NOT NULL,
+                        `title` varchar(255) NOT NULL,
+                        `content` text NOT NULL,
+                        `type` varchar(8) NOT NULL DEFAULT 'TEXT',
+                        `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                        PRIMARY KEY (`id`),
+                        KEY `author_id` (`author_id`),
+                        KEY `section_id` (`section_id`),
+                        CONSTRAINT `fk_post_section_section_id` FOREIGN KEY (`section_id`) REFERENCES `section` (`id`) ON DELETE CASCADE,
+                        CONSTRAINT `fk_post_user_author_id` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `post_vote`
+--
+
+DROP TABLE IF EXISTS `post_vote`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post_vote` (
+                             `post_id` int NOT NULL,
+                             `user_id` int NOT NULL,
+                             `vote` tinyint NOT NULL,
+                             PRIMARY KEY (`post_id`,`user_id`),
+                             KEY `user_id` (`user_id`),
+                             CONSTRAINT `fk_post_vote_post_post_id` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE,
+                             CONSTRAINT `fk_post_vote_user_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `section`
+--
+
+DROP TABLE IF EXISTS `section`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `section` (
+                           `id` int NOT NULL AUTO_INCREMENT,
+                           `description` varchar(255) DEFAULT NULL,
+                           `name` varchar(50) NOT NULL,
+                           `picture` varchar(4096) DEFAULT NULL,
+                           PRIMARY KEY (`id`),
+                           UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+                        `id` int NOT NULL AUTO_INCREMENT,
+                        `username` varchar(30) NOT NULL,
+                        `password` varchar(255) NOT NULL,
+                        `email` varchar(255) NOT NULL,
+                        `description` varchar(255) DEFAULT NULL,
+                        `picture` varchar(4096) DEFAULT NULL,
+                        `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                        PRIMARY KEY (`id`),
+                        UNIQUE KEY `username` (`username`),
+                        UNIQUE KEY `email` (`email`),
+                        UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-04 11:47:36
