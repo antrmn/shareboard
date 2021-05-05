856cc880fe7ddbc9361d8005dc4f8dc2006783b7
Antonio <ar0x2ff@gmail.com>
2021-05-05 11:22:03 +0200

    Added v_post view to db schema

--- SQL/createdb.sql
+++ SQL/createdb.sql
@@ -1,21 +1,19 @@
--- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
 --
--- Host: localhost    Database: shareboard
+-- Host: 127.0.0.1    Database: shareboard
 -- ------------------------------------------------------
 -- Server version	8.0.22
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-USE shareboard;
 
 --
 -- Table structure for table `admin`
@@ -54,7 +52,7 @@ CREATE TABLE `ban` (
                        CONSTRAINT `fk_ban_section_section_id` FOREIGN KEY (`section_id`) REFERENCES `section` (`id`) ON DELETE CASCADE,
                        CONSTRAINT `fk_ban_user_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
                        CONSTRAINT `global_or_local` CHECK ((((`is_global` = 1) and (`section_id` is null)) or ((`is_global` = 0) and (`section_id` is not null))))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -78,7 +76,7 @@ CREATE TABLE `comment` (
                            CONSTRAINT `fk_comment_comment_parent_comment_id` FOREIGN KEY (`parent_comment_id`) REFERENCES `comment` (`id`) ON DELETE CASCADE,
                            CONSTRAINT `fk_comment_post_post_id` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE,
                            CONSTRAINT `fk_comment_user_author_id` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -136,7 +134,7 @@ CREATE TABLE `post` (
                         KEY `section_id` (`section_id`),
                         CONSTRAINT `fk_post_section_section_id` FOREIGN KEY (`section_id`) REFERENCES `section` (`id`) ON DELETE CASCADE,
                         CONSTRAINT `fk_post_user_author_id` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -171,7 +169,7 @@ CREATE TABLE `section` (
                            `picture` varchar(4096) DEFAULT NULL,
                            PRIMARY KEY (`id`),
                            UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -195,6 +193,47 @@ CREATE TABLE `user` (
                         UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Temporary view structure for view `v_post`
+--
+
+DROP TABLE IF EXISTS `v_post`;
+/*!50001 DROP VIEW IF EXISTS `v_post`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `v_post` AS SELECT
+                                     1 AS `id`,
+                                     1 AS `title`,
+                                     1 AS `content`,
+                                     1 AS `type`,
+                                     1 AS `creation_date`,
+                                     1 AS `author_id`,
+                                     1 AS `username`,
+                                     1 AS `is_admin`,
+                                     1 AS `section_id`,
+                                     1 AS `section_name`,
+                                     1 AS `votes`,
+                                     1 AS `n_comments`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Final view structure for view `v_post`
+--
+
+/*!50001 DROP VIEW IF EXISTS `v_post`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+    /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+    /*!50001 VIEW `v_post` AS select distinct `post`.`id` AS `id`,`post`.`title` AS `title`,`post`.`content` AS `content`,`post`.`type` AS `type`,`post`.`creation_date` AS `creation_date`,`post`.`author_id` AS `author_id`,`user`.`username` AS `username`,(`a`.`user_id` is not null) AS `is_admin`,`sec`.`id` AS `section_id`,`sec`.`name` AS `section_name`,ifnull(`v`.`sum_votes`,0) AS `votes`,ifnull(`c`.`count_comments`,0) AS `n_comments` from (((((`post` join `section` `sec` on((`sec`.`id` = `post`.`section_id`))) join `user` on((`post`.`author_id` = `user`.`id`))) left join (select `post_vote`.`post_id` AS `post_id`,sum(`post_vote`.`vote`) AS `sum_votes` from `post_vote` group by `post_vote`.`post_id`) `v` on((`v`.`post_id` = `post`.`id`))) left join (select count(`comment`.`id`) AS `count_comments`,`comment`.`post_id` AS `post_id` from `comment` group by `comment`.`post_id`) `c` on((`c`.`post_id` = `post`.`id`))) left join `admin` `a` on((`post`.`author_id` = `a`.`user_id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -205,4 +244,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-04 11:47:36
+-- Dump completed on 2021-05-05 11:17:44
