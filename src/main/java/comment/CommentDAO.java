d5e884ae70757346a7398512fcd4c59a257177b6
Antonio <ar0x2ff@gmail.com>
2021-05-14 22:53:04 +0200

    CommentDAO: Aggiungo fetchHierarchy

--- src/main/java/comment/CommentDAO.java
+++ src/main/java/comment/CommentDAO.java
@@ -5,9 +5,8 @@ import persistence.StatementSetters;
 import util.Pair;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringJoiner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CommentDAO {
     private static final CommentMapper cm = new CommentMapper();
@@ -108,5 +107,70 @@ public class CommentDAO {
         int rowsDeleted = ps.executeUpdate();
         ps.close();
         return rowsDeleted;
+    }
+
+    public Map<Integer, ArrayList<Comment>> fetchHierarchy(int id, boolean isCommentId, int maxDepth, int loggedUserId) throws SQLException {
+        //Lista di colonne da restituire
+        List<String> columns = List.of("id AS comment_id", "content AS comment_content", "creation_date AS comment_creation_date",
+                "author_id AS user_id", "parent_comment_id", "username", "is_admin", "post_id", "post_title",
+                "section_id", "section_name", "votes AS comment_votes", "cte.vote AS comment_vote");
+        //Le stesse colonne di sopra, ma con il prefisso "com." (per la recursive CTE)
+        List<String> columnsRecurse = columns.stream().map(string -> "com."+string).collect(Collectors.toList());
+
+        List<Pair<Object,Integer>> params = new ArrayList<>();
+
+        //All'interno della recursive CTE è definita un'altra CTE (non ricorsiva) per aggiungere la colonna "voto" al risultati.
+        //Se l'utente è loggato verrà mostrato il voto al commento, altrimenti sarà restituito solo il valore '0' per quella colonna.
+        String CTE = "";
+        String baseCaseJoin = "CROSS JOIN (SELECT 0 AS vote) AS cte";
+        String recursiveStepJoin = "CROSS JOIN (SELECT 0 AS vote) AS cte";
+        if(loggedUserId > 0){
+            CTE = " WITH votes_from_user_cte AS (SELECT comment_id, vote, user_id FROM comment_vote JOIN user ON user_id=user.id WHERE user_id=?) ";
+            baseCaseJoin = "LEFT JOIN votes_from_user_cte AS cte ON cte.comment_id = comment_id";
+            recursiveStepJoin = "LEFT JOIN votes_from_user_cte AS cte ON cte.comment_id = com.comment_id";
+            params.add(new Pair<>(loggedUserId, Types.INTEGER));
+        }
+
+        //L'ordine di inserimento dei parametri conta!
+        if(isCommentId)
+            params.add(new Pair<>(id, Types.INTEGER));
+        if(maxDepth >= 0)
+            params.add(new Pair<>(maxDepth, Types.INTEGER));
+        if(!isCommentId)
+            params.add(new Pair<>(id, Types.INTEGER));
+
+        String query =
+                "WITH RECURSIVE Recurse_Comments AS ( "+
+                        " %s "+
+                        "SELECT 1 AS depth, %s FROM v_comment %s %s "+
+                        "UNION ALL "+
+                        "SELECT depth+1 AS depth, %s FROM v_comment AS com %s %s " +
+                        "JOIN Recurse_Comments AS r ON com.parent_comment_id = r.comment_id ) " +
+                        "SELECT * FROM Recurse_Comments %s";
+
+        query = String.format(query,
+                CTE,
+                String.join(",", columns),
+                baseCaseJoin,
+                isCommentId ? "WHERE id = ?" : "WHERE parent_comment_id IS NULL",
+                String.join(",", columnsRecurse),
+                recursiveStepJoin,
+                maxDepth >= 0 ? "WHERE depth <= ?-1" : " ",
+                isCommentId ? "" : "WHERE post_id = ?");
+
+        PreparedStatement ps = con.prepareStatement(query);
+        StatementSetters.setParameters(ps, params);
+        ResultSet rs = ps.executeQuery();
+
+        HashMap<Integer, ArrayList<Comment>> commentsMap = new HashMap<>();
+        while(rs.next()){
+            Comment comment = cm.toBean(rs);
+            int parentCommentId = comment.getParentComment().getId(); //Ricorda: JDBC restituisce 0 se una colonna di tipo INTEGER ha valore NULL.
+            if(!commentsMap.containsKey(parentCommentId)){
+                commentsMap.put(parentCommentId, new ArrayList<>());
+            }
+            commentsMap.get(parentCommentId).add(comment);
+        }
+        return commentsMap;
     }
 }
