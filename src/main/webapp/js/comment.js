567581772e2ed0406fc229717f62605ba0ec1348
Zane <alessandro.demaio@virgilio.it>
2021-06-23 20:30:27 +0200

    fix html voti

--- src/main/webapp/js/comment.js
+++ src/main/webapp/js/comment.js
@@ -3,10 +3,18 @@ $( document ).ready(function() {
     loadComments();
 });
 
-function createComment(author, votes, content, id, isEven, isSpecial){
+function createComment(author, votes, vote, content, id, isEven, isSpecial){
     let style = getComputedStyle(document.body);
     let color = "#242323" //make this a css var
 
+    let upvoteClass = "";
+    if (vote === 1){
+        upvoteClass = "upvote-icon-active"
+    }
+    let downvoteClass = "";
+    if (vote === -1){
+        downvoteClass = "downvote-icon-active";
+    }
     if (isEven){
         color = style.getPropertyValue('--shareboard-container-1');
     }
@@ -14,11 +22,11 @@ function createComment(author, votes, content, id, isEven, isSpecial){
               <div id = "${id}" class = "grid-x-nw" style = "width: 100%; align-items: start; margin-top:10px; background-color: ${color}; border-radius: 4px; border: solid 1px #313132; ">
                   <div class = "vote-container">
                     <input type = "hidden" name = "id" value = ${id}>
-                    <i class="fas fa-chevron-up voteIcon upvoteIcon interactable" onclick = "toggleVote(this, 'upvote', 'comment')"></i>
+                    <i class="fas fa-chevron-up voteIcon upvoteIcon interactable ${upvoteClass}" onclick = "toggleVote(this, 'upvote', 'comment')"></i>
                     <div class = "vote-count" style="word-break: initial; text-align: center; font-size: 12px;font-weight: 700; line-height: 16px;">
                         ${votes}
                     </div>
-                    <i class="fas fa-chevron-down voteIcon downvoteIcon interactable" onclick = "toggleVote(this, 'downvote', 'comment')"></i>
+                    <i class="fas fa-chevron-down voteIcon downvoteIcon interactable ${downvoteClass}" onclick = "toggleVote(this, 'downvote', 'comment')"></i>
                   </div>
                   <div class = "grid-y-nw" style="flex-grow:1; align-items: start;padding-bottom: 10px; padding-right: 10px;">
                       <div style = "flex-basis: 100%">
@@ -58,7 +66,7 @@ function addChilds(comments, key, depth){
             // depth++;
             console.log(depth + " " + comment.text);
             console.log(depth%2===0);
-            $(createComment(comment.author.username, comment.vote, comment.text, comment.id, depth%2===0)).insertAfter(`#${key} #reply-button`)
+            $(createComment(comment.author.username, comment.votes,comment.vote, comment.text, comment.id, depth%2===0)).insertAfter(`#${key} #reply-button`)
             addChilds(comments, comment.id, depth++);
         }
     }
@@ -86,7 +94,7 @@ function loadComments(){
             let baseId = 0;
             if (comments[baseId]){
                 for(let comment of comments[baseId]){
-                    $("#comments-container").append(createComment(comment.author.username, comment.vote, comment.text, comment.id, false));
+                    $("#comments-container").append(createComment(comment.author.username, comment.votes,comment.vote, comment.text, comment.id, false));
                     addChilds(comments, comment.id, 1);
                 }
             }
