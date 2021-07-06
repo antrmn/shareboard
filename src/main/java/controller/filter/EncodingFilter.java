6f2c3f046d28fb967baa9dbb3df2b629fa068dab
Antonio <ar0x2ff@gmail.com>
2021-07-06 19:08:47 +0200

    fix encoding issues

---
+++ src/main/java/controller/filter/EncodingFilter.java
@@ -0,0 +1,20 @@
+package controller.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/* Il modo più veloce per fixare problemi con accenti e roba del genere */
+@WebFilter(filterName = "encodingFilter")
+public class EncodingFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        req.setCharacterEncoding("UTF-8");
+        res.setCharacterEncoding("UTF-8");
+        chain.doFilter(req, res);
+    }
+}
