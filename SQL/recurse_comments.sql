8779e69c064a7093f879f4e16f23fa518cd8e7f2
Antonio <ar0x2ff@gmail.com>
2021-07-26 12:34:11 +0200

    aggiunte query recurse_comments.sql

--- SQL/recurse_comments.sql
+++ SQL/recurse_comments.sql
@@ -1,3 +1,120 @@
+/* Guest, vedi commenti post */
+WITH RECURSIVE Recurse_Comments AS (
+    SELECT
+        0 AS depth
+         , vcc.*
+         , cte.vote
+    FROM v_comment_complete AS vcc
+             CROSS JOIN (SELECT 0 AS vote) AS cte
+    WHERE parent_comment_id IS NULL
+    UNION ALL
+    SELECT
+            depth+1 AS depth
+         , com.*
+         , cte.vote
+    FROM v_comment_complete AS com
+             CROSS JOIN (SELECT 0 AS vote) AS cte
+             JOIN Recurse_Comments AS r
+                  ON com.parent_comment_id = r.id
+    WHERE depth <= ?
+)
+SELECT * FROM Recurse_Comments WHERE post_id = ?;
+
+
+/* Guest, vedi risposte ai commenti */
+WITH RECURSIVE Recurse_Comments AS (
+    SELECT
+        0 AS depth
+        , vcc.*
+        , cte.vote
+    FROM v_comment_complete AS vcc
+    CROSS JOIN (SELECT 0 AS vote) AS cte
+    WHERE id = 71
+    UNION ALL
+        SELECT
+            depth+1 AS depth
+            , com.*
+            , cte.vote
+        FROM v_comment_complete AS com
+        CROSS JOIN (SELECT 0 AS vote) AS cte
+        JOIN Recurse_Comments AS r
+            ON com.parent_comment_id = r.id
+        WHERE depth <= 2
+)
+SELECT * FROM Recurse_Comments;
+
+
+/* Logged user, vedi commenti a post */
+WITH RECURSIVE Recurse_Comments AS (
+    WITH votes_from_user_cte AS (
+        SELECT
+            comment_id
+            , vote
+            , user_id
+        FROM comment_vote
+        JOIN user
+            ON user_id=user.id
+        WHERE user_id=17
+    )
+    SELECT
+        0 AS depth
+        , vcc.*
+        , cte.vote
+    FROM v_comment_complete AS vcc
+    LEFT JOIN votes_from_user_cte AS cte
+        ON cte.comment_id = vcc.id
+    WHERE parent_comment_id IS NULL
+    UNION ALL
+        SELECT
+            depth+1 AS depth
+            , com.*
+            , cte.vote
+        FROM v_comment_complete AS com
+        LEFT JOIN votes_from_user_cte AS cte
+            ON cte.comment_id = com.id
+        JOIN Recurse_Comments AS r
+            ON com.parent_comment_id = r.id
+        WHERE depth <= 2
+)
+SELECT * FROM Recurse_Comments  WHERE post_id = 58;
+
+
+/* guest, vedi risposte ai commenti */
+WITH RECURSIVE Recurse_Comments AS (
+    WITH votes_from_user_cte AS (
+        SELECT
+            comment_id
+            , vote
+            , user_id
+        FROM comment_vote
+        JOIN user
+            ON user_id=user.id WHERE user_id=17
+    )
+    SELECT
+        0 AS depth
+        , vcc.*
+        , cte.vote
+    FROM v_comment_complete AS vcc
+    LEFT JOIN votes_from_user_cte AS cte
+        ON cte.comment_id = vcc.id
+    WHERE id = 73
+    UNION ALL
+        SELECT
+            depth+1 AS depth
+            , com.*
+            , cte.vote
+        FROM v_comment_complete AS com
+        LEFT JOIN votes_from_user_cte AS cte
+            ON cte.comment_id = com.id
+        JOIN Recurse_Comments AS r
+            ON com.parent_comment_id = r.id
+        WHERE depth <= 2
+)
+SELECT * FROM Recurse_Comments;
+
+
+
+/* CONCEPT */
 WITH RECURSIVE Recurse_Comments AS (
     SELECT * FROM comment WHERE parent_comment_id IS NULL
     UNION ALL
